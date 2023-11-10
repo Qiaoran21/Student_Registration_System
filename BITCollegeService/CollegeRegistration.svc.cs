431fb45666076ee4177296b289b9d154c6d994e3
Qiaoran21 <qiaoran@hotmail.com>
2023-11-10 08:59:22 -0600

    Add project files.

---
+++ BITCollegeService/CollegeRegistration.svc.cs
@@ -0,0 +1,198 @@
+﻿using BITCollege_QX.Data;
+using BITCollege_QX.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+using System.Web.Security;
+using System.Web.UI.WebControls;
+using Utility;
+
+namespace BITCollegeService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "CollegeRegistration" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select CollegeRegistration.svc or CollegeRegistration.svc.cs at the Solution Explorer and start debugging.
+    public class CollegeRegistration : ICollegeRegistration
+    {
+        BITCollege_QXContext db = new BITCollege_QXContext();
+
+        int returnValue = 0;
+
+        //public void DoWork()
+        //{
+        //}
+
+        /// <summary>
+        /// DropCourse method in CollegeRegistration.
+        /// </summary>
+        /// <param name="registrationId">registrationId</param>
+        /// <returns>True if the course registration was successfully dropped. 
+        /// False if it failed or the specified registrationId was not found.</returns>
+        public bool DropCourse(int registrationId)
+        {
+            Registration registrations = db.Registrations.Where(x=>x.RegistrationId == registrationId).FirstOrDefault();
+
+            try
+            {
+                if (registrations != null)
+                {
+                    db.Registrations.Remove(registrations);
+                    db.SaveChanges();
+                    return true;
+                }
+                return false;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// RegisterCourse method in CollegeRegistration.
+        /// </summary>
+        /// <param name="studentId">studentId</param>
+        /// <param name="courseId">courseId</param>
+        /// <param name="notes">notes</param>
+        /// <returns>localValue</returns>
+        public int RegisterCourse(int studentId, int courseId, string notes)
+        {
+            //Retrieve all records from Registrations table involving Student and Course.
+            IQueryable<Registration> registrations = db.Registrations.Where(x=>x.StudentId == studentId 
+                                                       && x.CourseId == courseId && x.Notes == notes);
+
+            //Retrieve Course record.
+            Course course = db.Courses.Where(x => x.CourseId == courseId && x.Notes == notes).FirstOrDefault();
+            //Retrieve Student record.
+            Student student = db.Students.Where(x => x.StudentId == studentId && x.Notes == notes).FirstOrDefault();
+
+            //Query the allRegistrations query - null records.
+            IEnumerable<Registration> nullRecords = db.Registrations.Where(x => x.Grade == null);
+            if (nullRecords.Count() > 0)
+            {
+                returnValue = -100;
+            }
+
+            //Check to see if course is Mastery course, and get max attempts.
+            CourseType type = BusinessRules.CourseTypeLookup(course.CourseType);
+
+            if (type == CourseType.MASTERY)
+            {
+                MasteryCourse masteryCourse = (MasteryCourse)course;
+                int maximumAttempts = masteryCourse.MaximumAttempts;
+
+                //Query the allRegistrations query - not null records.
+                IEnumerable<Registration> notNullRecords = db.Registrations.Where(x => x.Grade != null);
+                if (notNullRecords.Count() > maximumAttempts)
+                {
+                    returnValue = -200;
+                }
+            }
+            
+            try
+            {
+                Registration registration = new Registration();
+
+                registration.StudentId =studentId;
+                registration.CourseId = courseId;
+                registration.Notes = notes;
+                registration.RegistrationDate = DateTime.Today;
+                registration.SetNextRegistrationNumber();
+
+                db.Registrations.Add(registration);
+                db.SaveChanges();
+
+                double tuitionAmount = course.TuitionAmount;
+
+                double adjustedTuitionAmount = student.GradePointState.TuitionRateAdjustment(student);
+
+                student.OutstandingFees += adjustedTuitionAmount;
+
+                db.SaveChanges();
+            }
+            catch (Exception)
+            {
+                returnValue = -300;
+            }
+
+            return returnValue;
+        }
+
+        /// <summary>
+        /// UpdateGrade method in CollegeRegistration. 
+        /// </summary>
+        /// <param name="grade">grade</param>
+        /// <param name="registrationId">registrationId</param>
+        /// <param name="notes">notes</param>
+        /// <returns>newGradePointAverage</returns>
+        public double? UpdateGrade(double grade, int registrationId, string notes)
+        {
+            Registration registration = db.Registrations.Where(x => x.RegistrationId == registrationId && x.Notes == notes).FirstOrDefault();
+
+            registration.Grade = grade;
+
+            registration.Notes = notes;
+
+            db.SaveChanges();
+
+            double? newGradePointAverage = CalculateGradePointAverage(registration.StudentId);
+
+            return newGradePointAverage;
+        }
+
+        /// <summary>
+        /// CalculateGradePointAverage method in CollegeRegistration.
+        /// </summary>
+        /// <param name="studentId">studentId</param>
+        /// <returns>calcuatedGreadePointAverage</returns>
+        private double? CalculateGradePointAverage(int studentId)
+        {
+            double grade;
+            CourseType courseType;
+            double gradePoint;
+            double gradePointValue;
+            double totalCreditHours = 0.0;
+            double totalGradePointValue = 0.0;
+            double? calcuatedGreadePointAverage;
+
+            IQueryable<Registration> registrations = db.Registrations.Where(x => x.Grade != null 
+                                                            && x.StudentId == studentId);
+
+            foreach (Registration registration in registrations.ToList())
+            {
+                grade = registration.Grade.Value;
+
+                courseType = BusinessRules.CourseTypeLookup(registration.Course.CourseType);
+
+                if (courseType != CourseType.AUDIT)
+                {
+                    gradePoint = BusinessRules.GradeLookup(grade, courseType);
+
+                    gradePointValue = gradePoint * registration.Course.CreditHours;
+
+                    totalGradePointValue += gradePointValue;
+
+                    totalCreditHours += registration.Course.CreditHours;
+                }
+            }
+
+            if (totalCreditHours == 0)
+            {
+                calcuatedGreadePointAverage = null;
+            } else
+            {
+                calcuatedGreadePointAverage = totalGradePointValue / totalCreditHours;
+            }
+
+            Student student = db.Students.Where(x => x.StudentId == studentId && x.GradePointAverage == calcuatedGreadePointAverage).FirstOrDefault();
+
+            db.SaveChanges();
+
+            return calcuatedGreadePointAverage;
+        }
+    }
+}
---
+++ TuitionRateAdjustmentTest/Program.cs
@@ -0,0 +1,234 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BITCollege_QX.Models;
+using BITCollege_QX.Data;
+
+
+namespace BITCollege_QX.TuitionRateAdjustmentTest
+{
+    public class Program
+    {
+        private static BITCollege_QXContext db = new BITCollege_QXContext();
+
+        static void Main(string[] args)
+        {
+            Suspended_Newly_Registered_44();
+            Suspended_Newly_Registered_60();
+            Suspended_Newly_Registered_80();
+
+            Probation_Newly_Registered_3_Courses();
+            Probation_Newly_Registered_7_Courses();
+
+            Regular_Newly_Registered_250();
+
+            Honours_Newly_Registered_3_Courses();
+            Honours_Newly_Registered_4_Courses();
+            Honours_Newly_Registered_7_Courses();
+            Honours_Newly_Registered_7_Courses_2();
+        }
+
+        /// <summary>
+        /// Test for when GPA is 0.44. 
+        /// </summary>
+        static void Suspended_Newly_Registered_44()
+        {
+            Student student = db.Students.Find(3);
+            student.GradePointAverage = 0.44;
+            student.GradePointStateId = 1;
+            db.SaveChanges();
+
+            GradePointState state = db.GradePointStates.Find(student.GradePointStateId);
+
+            double tuitionRate = 1000 * state.TuitionRateAdjustment(student);
+
+            Console.WriteLine("Suspended State:");
+            Console.WriteLine("Expected tuition: 1150");
+            Console.WriteLine("Actual tuition: " + tuitionRate);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Test for when GPA is 0.60.
+        /// </summary>
+        static void Suspended_Newly_Registered_60()
+        {
+            Student student = db.Students.Find(3);
+            student.GradePointAverage = 0.60;
+            student.GradePointStateId = 1;
+            db.SaveChanges();
+
+            GradePointState state = db.GradePointStates.Find(student.GradePointStateId);
+
+            double tuitionRate = 1000 * state.TuitionRateAdjustment(student);
+
+            Console.WriteLine("Expected tuition: 1120");
+            Console.WriteLine("Actual tuition: " + tuitionRate);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Test for when GPA is 0.80.
+        /// </summary>
+        static void Suspended_Newly_Registered_80()
+        {
+            Student student = db.Students.Find(3);
+            student.GradePointAverage = 0.80;
+            student.GradePointStateId = 1;
+            db.SaveChanges();
+
+            GradePointState state = db.GradePointStates.Find(student.GradePointStateId);
+
+            double tuitionRate = 1000 * state.TuitionRateAdjustment(student);
+
+            Console.WriteLine("Expected tuition: 1100");
+            Console.WriteLine("Actual tuition: " + tuitionRate);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Test for when GPA is 1.15 and has complete 3 courses.
+        /// </summary>
+        static void Probation_Newly_Registered_3_Courses()
+        {
+            Student student = db.Students.Find(3);
+            student.GradePointAverage = 1.15;
+            student.GradePointStateId = 5;
+            db.SaveChanges();
+
+            GradePointState state = db.GradePointStates.Find(student.GradePointStateId);
+
+            double tuitionRate = 1000 * state.TuitionRateAdjustment(student);
+
+            Console.WriteLine("Probation State:");
+            Console.WriteLine("Expected tuition: 1075");
+            Console.WriteLine("Actual tuition: " + tuitionRate);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Test for when GPA is 1.15 and has complete 7 courses.
+        /// </summary>
+        static void Probation_Newly_Registered_7_Courses()
+        {
+            Student student = db.Students.Find(2);
+            student.GradePointAverage = 1.15;
+            student.GradePointStateId = 5;
+            db.SaveChanges();
+
+            GradePointState state = db.GradePointStates.Find(student.GradePointStateId);
+
+            double tuitionRate = 1000 * state.TuitionRateAdjustment(student);
+
+            Console.WriteLine("Expected tuition: 1035");
+            Console.WriteLine("Actual tuition: " + tuitionRate);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Test when GPA is 2.5.
+        /// </summary>
+        static void Regular_Newly_Registered_250()
+        {
+            Student student = db.Students.Find(2);
+            student.GradePointAverage = 2.50;
+            student.GradePointStateId = 6;
+            db.SaveChanges();
+
+            GradePointState state = db.GradePointStates.Find(student.GradePointStateId);
+
+            double tuitionRate = 1000 * state.TuitionRateAdjustment(student);
+
+            Console.WriteLine("Regular State:");
+            Console.WriteLine("Expected tuition: 1000");
+            Console.WriteLine("Actual tuition: " + tuitionRate);
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Test for when GPA is 3.9 and has complete 3 courses.
+        /// </summary>
+        static void Honours_Newly_Registered_3_Courses()
+        {
+            Student student = db.Students.Find(3);
+            student.GradePointAverage = 3.90;
+            student.GradePointStateId = 7;
+            db.SaveChanges();
+
+            GradePointState state = db.GradePointStates.Find(student.GradePointStateId);
+
+            double tuitionRate = 1000 * state.TuitionRateAdjustment(student);
+
+            Console.WriteLine("Honours State:");
+            Console.WriteLine("Expected tuition: 900");
+            Console.WriteLine("Actual tuition: " + tuitionRate);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Test for when GPA is 4.27 and has complete 4 courses.
+        /// </summary>
+        static void Honours_Newly_Registered_4_Courses()
+        {
+            Student student = db.Students.Find(3);
+            student.GradePointAverage = 4.27;
+            student.GradePointStateId = 7;
+            db.SaveChanges();
+
+            GradePointState state = db.GradePointStates.Find(student.GradePointStateId);
+
+            double tuitionRate = 1000 * state.TuitionRateAdjustment(student);
+
+            Console.WriteLine("Expected tuition: 880");
+            Console.WriteLine("Actual tuition: " + tuitionRate);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Test for when GPA is 4.40 and has complete 7 courses.
+        /// </summary>
+        static void Honours_Newly_Registered_7_Courses()
+        {
+            Student student = db.Students.Find(2);
+            student.GradePointAverage = 4.40;
+            student.GradePointStateId = 7;
+            db.SaveChanges();
+
+            GradePointState state = db.GradePointStates.Find(student.GradePointStateId);
+
+            double tuitionRate = 1000 * state.TuitionRateAdjustment(student);
+
+            Console.WriteLine("Expected tuition: 830");
+            Console.WriteLine("Actual tuition: " + tuitionRate);
+            Console.WriteLine();
+        }
+
+        /// <summary>
+        /// Test for when GPA is 4.10 and has complete 7 courses.
+        /// </summary>
+        static void Honours_Newly_Registered_7_Courses_2()
+        {
+            Student student = db.Students.Find(2);
+            student.GradePointAverage = 4.10;
+            student.GradePointStateId = 7;
+            db.SaveChanges();
+
+            GradePointState state = db.GradePointStates.Find(student.GradePointStateId);
+
+            double tuitionRate = 1000 * state.TuitionRateAdjustment(student);
+
+            Console.WriteLine("Expected tuition: 850");
+            Console.WriteLine("Actual tuition: " + tuitionRate);
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
